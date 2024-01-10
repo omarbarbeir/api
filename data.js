4878c43285861022e24c555fcc1ef46efb932f4b
omarbarbeir <138704845+omarbarbeir@users.noreply.github.com>
2024-01-10 21:30:54 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,120 @@
+// export let data =[
+//   {
+//     id:"1",
+//     title:"Computer",
+//     price:"1650",
+//     image:"https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//   },
+//   {
+//     id:"2",
+//     title:"BWM",
+//     price:"160000",
+//     image:"https://images.pexels.com/photos/100650/pexels-photo-100650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//   },
+//   {
+//     id:"3",
+//     title:"History Book",
+//     price:"780",
+//     image:"https://images.pexels.com/photos/762687/pexels-photo-762687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//   },
+//   {
+//     id:"4",
+//     title:"Iphone",
+//     price:"3750",
+//     image:"https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//   }
+// ]
+
+export let data = [
+  {
+      id: "1",
+      title: "Apple iPhone 11 with FaceTime - 128 GB, 4 GB RAM, 4G LTE, white, single SIM and e-SIM",
+      category: "phone",
+       type: "iphone",
+       price : "919",
+       image: require("./images/iphone.png"),
+   },
+   {
+      id: "2",
+      title: "Samsung Galaxy M34 mobile phone, dual SIM, 8 GB RAM, 128 GB storage, 5G",
+      category: "phone",
+       type: "samsung",
+       price : "320",
+       image : require("./images/samsung.png")
+   },
+   {
+      id: "3",
+      title: "Oppo A18 mobile phone with 128 GB storage, 4 GB RAM, 4G network, glowing blue color - dual SIM",
+      category: "phone",
+       type: "oppo",
+       price : "209.69",
+       image: require("./images/oppo.png")
+   },
+   {
+      id: "4",
+      title: "Jack & Jones Mens  Jortrevor Polo T-Shirt",
+      category: "clothes",
+      type: "men clothes",
+      price : "919",
+      image: require("./images/t-shirt.png"),
+   },
+  {
+      id: "5",
+      title: "Men's elastic polo T-shirt from American Eagle",
+      category: "clothes",
+       type: "men clothes",
+       price : "12.94",
+       image: require("./images/t-shirt2.png")
+   },
+   {
+      id: "6",
+      title: "High Cool Winter Women (High Neck Pullover for Women)",
+      category: "clothes",
+       type: "women clothes",
+       price : "7.28",
+       image: require("./images/highcool.png")
+   },
+   {
+      id: "7",
+      title: "Milton winter sports set for women consisting of a turtleneck hoodie and long pants",
+      category: "clothes",
+       type: "women clothes",
+       price : "16.50",
+       image: require("./images/milton.png")
+   },
+   {
+      id: "8",
+      title: "HP Laptop 15S-FQ5048NE (6L8K8EA), Intel Core i3 - 1215U, 12th Gen, 4GB DDR4, 256GB SSD, Windows 11 Home Single Language",
+      category: "electronics",
+       type: "laptop",
+       price : "453.05",
+       image: require("./images/hp laptop.png")
+   },
+   {
+      id: "9",
+      title: "Dell G15 5520 Gaming Laptop, Intel Core i7 12th Gen, 14-Core 12700H, 16GB DDR5 4800Mhz RAM, 1TB SSD, NVIDIA GeForce RTX 3060 GDDR6 Graphics, 15.6 Inch FHD(1920 x 1080)165Hz, Backlit Keyboard, Windows 11",
+      category: "electronics",
+       type: "laptop",
+       price : "1974",
+       image: require("./images/dell laptop.png"),
+   },
+   {
+      id: "10",
+      title: "Samsung 55 Inch LED 4K Crystal Ultra HD Smart TV with Built-in Receiver - UA55CU7000UXEG, Black",
+      category: "tv",
+       type: "samsung",
+       price : "533.95",
+       image: require("./images/samsung tv.png")
+   },
+   {
+      id: "11",
+      title: "Tornado 65-inch Smart 4K UHD TV with Android system, frameless, with remote control and Wi-Fi connectivity",
+      category: "tv",
+       type: "tornado",
+       price : "533.95",
+       image: require("./images/tornado tv.png")
+   }
+  
+
+]
+export default data
